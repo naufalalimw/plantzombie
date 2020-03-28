383b38f434da1cc2c3e96445001d136851bd8f18
kevincahyadigiri <kevincahyadigiri@gmail.com>
2020-03-28 10:54:23 +0700

    update

--- Plant.java
+++ Plant.java
@@ -1,28 +1,49 @@
 public abstract class Plant implements Creature{
-    public int HP;
-    public Point position;
-    public int attack;
+    private int hp;
+    private Point position;
+    private int attack;
 
     //Implementasi Interface
     public void takeDamage(int damage){
-        this.HP = this.HP - damage;
+        this.hp = this.hp - damage;
     }
     
+    //getter
     public int getHP(){
-        return this.HP;
+        return this.hp;
     }
     
     public int getAttack(){
         return this.attack;
     }
 
-    public boolean isDead(){
-        return (this.HP <= 0);
-    }
-
     public Point getPosition(){
         return this.position;
     }
+
+    //setter
+    // public void setHP(int hp){
+    //     this.hp = hp;
+    // }
+
+    public void setPosition(Point p){
+        this.position = p;
+    }
+
+    public void setPosition(int x, int y){
+        Point newPosition = new Point(x,y);
+        this.position = newPosition;
+    }
+
+    public void setAttack(int att){
+        this.attack = att;
+    }
+    
+    //methods
+    public boolean isDead(){
+        return (this.hp <= 0);
+    }
+    
     //abstract Methods
     public abstract void attack(); //method untuk menyerang
 
